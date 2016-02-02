9123a5da57b04e71e9b7035147853ea4e229bd66
karrakunga <karrakunga@gmail.com>
2016-02-02 17:07:20 +0000

    Added rest client

---
+++ src/Core/ApiClient/ApiClient.cs
@@ -0,0 +1,28 @@
+﻿namespace api.boilerplate.Core.ApiClient
+{
+    public interface IApiClient
+    {
+        T Get<T>(string endpoint);
+    }
+
+    public class ApiClient : IApiClient
+    {
+        private readonly IRestClientFactory _restClientFactory;
+
+        public ApiClient() : this(new RestClientFactory())
+        {
+        }
+
+        public ApiClient(IRestClientFactory restClientFactory)
+        {
+            _restClientFactory = restClientFactory;
+        }
+
+        public T Get<T>(string endpoint)
+        {
+            return _restClientFactory
+                .CreateApiClient()
+                .Get<T>(endpoint);
+        }
+    }
+}
